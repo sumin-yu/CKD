1d68796b92def7a3b1783e0694c9172bdd2d5573
sangwon79 <toto6599@naver.com>
2023-10-05 16:39:17 +0900

    ff;

--- trainer/lbc.py
+++ trainer/lbc.py
@@ -149,18 +149,7 @@ class Trainer(trainer.GenericTrainer):
                     targets = targets.cuda()
 
                 if model != None:
-                    if self.data == 'jigsaw':
-                        input_ids = inputs[:, :, 0]
-                        input_masks = inputs[:, :, 1]
-                        segment_ids = inputs[:, :, 2]
-                        outputs = model(
-                            input_ids=input_ids,
-                            attention_mask=input_masks,
-                            token_type_ids=segment_ids,
-                            labels=targets,
-                        )[1] 
-                    else:
-                        outputs = model(inputs)
+                    outputs = model(inputs)
                     pred_set.append(torch.argmax(outputs, dim=1))
                 total+= inputs.shape[0]
 
